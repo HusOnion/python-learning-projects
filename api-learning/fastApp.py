4bb6e29e62f42a894aca29511e0cc8760c1b9570
Husam <sawaftah.husam@gmail.com>
2026-06-15 17:54:04 +0300

    learned more about paths + pydantic

--- api-learning/fastApp.py
+++ api-learning/fastApp.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 # we run it through uvicorn
 app = FastAPI()
+
+class Task(BaseModel):
+    id: int
+    title: str
+    isDone: bool
+
 
 #same as flask , for the root exept we use get as GET request not . route
 @app.get("/")
@@ -9,9 +16,11 @@ def main():
 
 tasks = []
 
+
+# so here when we send the item we want to send it in json format and it will be converted to a Task object using pydantic, not in a query parameter or form data, but in the body of the request as json
 #this is a route for tasks , to create the task , using POST
 @app.post("/tasks")
-def create_task(task):
+def create_task(task: Task): # this will convert the json to a task object using pydantic
     # add the task to the list 
     tasks.append(task)
     # so it will return the whole tasks
@@ -19,11 +28,15 @@ def create_task(task):
 
 # sending the post to the /tasks?task="eating" , while using the parameter to define the task
 
+@app.get("/tasks")
+def get_tasks(limit: int = 10): # this will limit the number of tasks to 10 by default
+    return tasks[0:limit] # this wll return the first 10 tasks in the list by default
+
 #this will get the task by its id (it place in the list)
 @app.get("/tasks/{task_id}")
-def get_task(task_id: int): # this will convert the id to int or we could convert it manually (task_id = int(task_id))
+def get_task(task_id: int) -> Task: # this will convert the task_id to an int and return a Task object
     # we will check if the task is in the list or not
-    if task_id > len(tasks): 
+    if task_id >= len(tasks): 
         raise HTTPException(status_code=404, detail="Task not found")
     task = tasks[task_id] #serching the list by the index
-    return task
+    return task 
